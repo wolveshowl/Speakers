5c5b6069db4d208cc8bb89cf8c2db512ac528270
Kyu0 <efftar@naver.com>
2023-03-25 13:54:57 +0900

    [add] 프로젝트 초기화

---
+++ src/main/java/com/note/speakers/SpeakersApplication.java
@@ -0,0 +1,13 @@
+package com.note.speakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeakersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/note/speakers/SpeakersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.speakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
